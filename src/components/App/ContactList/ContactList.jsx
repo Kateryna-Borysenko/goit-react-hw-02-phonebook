a54c4ec5a0cbd0110d8676629a8dba2a856e1682
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2021-12-10 17:19:41 +0200

    Added Paper for better style

--- src/components/App/ContactList/ContactList.jsx
+++ src/components/App/ContactList/ContactList.jsx
@@ -1,19 +1,22 @@
 import s from './ContactList.module.css';
+import Paper from '../common/Paper/Paper';
 
 const ContactList = ({ contacts, onDelete }) => (
   <ul className={s.contactList}>
     {contacts.map(({ id, name, number }) => (
-      <li key={id} className={s.contactListItem}>
-        <span className={s.contactName}>{name}</span>: {number}
-        <button
-          type="button"
-          className={s.button}
-          // первдала в props onDelete={this.deleteContact}
-          onClick={() => onDelete(id)}
-        >
-          Delete
-        </button>
-      </li>
+      <Paper>
+        <li key={id} className={s.contactListItem}>
+          <span className={s.contactName}>{name}</span>: {number}
+          <button
+            type="button"
+            className={s.button}
+            // первдала в props onDelete={this.deleteContact}
+            onClick={() => onDelete(id)}
+          >
+            Delete
+          </button>
+        </li>
+      </Paper>
     ))}
   </ul>
 );
